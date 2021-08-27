d2c187435f759d14681302ff6d43a4532ec3499d
Rhuan <cavalcante.d12@gmail.com>
2021-08-27 11:27:33 -0400

    next.config.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,45 @@
+import {
+  Container,
+  Box,
+  Input,
+  Button,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from "@chakra-ui/react";
 import { Logo } from "./../components";
 
 export default function Home() {
   return (
-    <div>
+    <Container p={4} centerContent>
       <Logo />
-    </div>
+      <Box p={4} mt={8}>
+        <text>Crie su agenda compartilhada</text>
+      </Box>
+
+      <Box>
+        <FormControl id="email" p={4} isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
+
+        <FormControl id="password" p={4} isRequired>
+          <FormLabel>Senha</FormLabel>
+          <Input type="password" />
+        </FormControl>
+
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <Text>clocker.work</Text>
+          <FormControl id="username" p={4} isRequired>
+            <Input type="username" />
+          </FormControl>
+        </Box>
+
+        <Box p={4}>
+          <Button width="100%">Entrar</Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
